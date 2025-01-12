8354b806b5c9c365b02411dc4bbffb8939e9e904
ajr <reanojohn@gmail.com>
2025-01-13 06:47:44 +1000

    Define Hero Section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import NavBar from "./components/nav/NavBar";
-
+import HeroSection from "./components/hero/HeroSection";
 function App() {
   return (
     <>
       <NavBar />
+      <HeroSection />
     </>
   );
 }
---
+++ src/components/hero/HeroSection.jsx
@@ -0,0 +1,24 @@
+import { pinotNoir } from "@/utils/assets";
+import { Button } from "../ui/button";
+import { Link } from "@chakra-ui/react";
+
+const HeroSection = () => {
+  return (
+    <main className="pt-[80px] bg-gray-300 h-1/2">
+      <div className="h-[70vh]">
+        <div className="mx-auto px-4 h-full flex flex-col justify-center items-center">
+          <h1 className="text-5xl font-bold text-white mb-4">
+            Discover Exquisite Wines
+          </h1>
+          <p className="text-xl text-white mb-8">
+            Curated selection of the finest wines from around the world
+          </p>
+          <Button as="a" href="#" className="bg-gray-400">
+            Explore Our Collection
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default HeroSection;
