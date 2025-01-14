c0525cccec94173d3bc233a22cf4f35a11875de7
ajr <reanojohn@gmail.com>
2025-01-14 22:04:52 +1000

    Refactor to Chakra UI syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import NavBar from "./components/nav/NavBar";
 import HeroSection from "./components/hero/HeroSection";
-import ProductSection from "./components/ProductSection";
+import CategorySection from "./components/CategorySection";
 function App() {
   return (
     <>
       <NavBar />
       <HeroSection />
-      <ProductSection />
+      <CategorySection />
     </>
   );
 }
