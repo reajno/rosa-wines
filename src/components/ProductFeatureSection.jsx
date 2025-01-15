89e9c0f78a7dacb7b7d0fc0eaa4f3cc417c6d0ef
ajr <reanojohn@gmail.com>
2025-01-15 11:49:51 +1000

    Set initial layout

---
+++ src/components/ProductFeatureSection.jsx
@@ -0,0 +1,13 @@
+import { Box, Container } from "@chakra-ui/react";
+import Carousel from "./Carousel";
+
+const ProductFeatureSection = () => {
+  return (
+    <Box as="section" bgColor={"green.200"}>
+      <Container py="4rem">
+        <Carousel />
+      </Container>
+    </Box>
+  );
+};
+export default ProductFeatureSection;
