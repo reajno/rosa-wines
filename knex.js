5eadbaaac57ccb2843fd9fd67ee84b2b8c0954ad
ajr <reanojohn@gmail.com>
2025-01-17 21:19:14 +1000

    Remove whitespace

---
+++ knex.js
@@ -0,0 +1,17 @@
+import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = knex({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+export default db;
