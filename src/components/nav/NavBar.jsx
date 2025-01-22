8627e75bce9474c61528fd548d0b88e9f29a6c6a
ajr <reanojohn@gmail.com>
2025-01-22 15:25:41 +1000

    Refactor to include element spacer

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import NavMenuSmall from "./NavMenuSmall";
 import NavMenuLarge from "./navMenuLarge";
 import BrandLogo from "../BrandLogo";
-import Cart from "./Cart";
+import CartToggle from "../Cart/CartToggle";
 import Account from "./Account";
 import useWindowSize from "../../hooks/useWindowSize";
 import { Container, Box, HStack, AbsoluteCenter } from "@chakra-ui/react";
@@ -9,33 +9,37 @@ const NavBar = () => {
   const { width } = useWindowSize();
 
   return (
-    <Container
-      as="header"
-      fluid
-      position="fixed"
-      zIndex={50}
-      bgColor={"red.400"}
-      py={6}
-      px={{ md: "14" }}
-    >
-      <HStack
-        mx="auto"
-        as="nav"
-        position="relative"
-        alignItems="center"
-        justify="space-between"
-        maxW={"1440px"}
+    <>
+      <Container
+        as="header"
+        fluid
+        position="fixed"
+        zIndex={50}
+        bgColor={"red.400"}
+        py={4}
+        px={{ md: "14" }}
       >
-        {width > 992 ? <NavMenuLarge /> : <NavMenuSmall />}
-        <AbsoluteCenter mx="auto" justifyContent="center">
-          <BrandLogo />
-        </AbsoluteCenter>
-        <HStack>
-          <Account />
-          <Cart />
+        <HStack
+          mx="auto"
+          as="nav"
+          position="relative"
+          alignItems="center"
+          justify="space-between"
+          maxW={"1440px"}
+        >
+          {width > 992 ? <NavMenuLarge /> : <NavMenuSmall />}
+          <AbsoluteCenter mx="auto" justifyContent="center">
+            <BrandLogo />
+          </AbsoluteCenter>
+          <HStack>
+            <Account />
+            <CartToggle />
+          </HStack>
         </HStack>
-      </HStack>
-    </Container>
+      </Container>
+      {/* NAV SPACER */}
+      <Box h="4.5rem" w="100%" />
+    </>
   );
 };
 export default NavBar;
