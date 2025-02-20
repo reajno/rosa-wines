50ba04bd867e154101d7c5f78cb53ff2e948eabd
ajr <reanojohn@gmail.com>
2025-02-20 21:32:22 +1000

    Attempt fix issues

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  optimizeDeps: {
+    include: ["@chakra-ui/react"],
+  },
+  build: {
+    rollupOptions: {
+      external: ["@chakra-ui/react"],
+    },
+  },
 });
